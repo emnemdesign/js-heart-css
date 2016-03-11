990916adf1aefea03371fb9f140bd5a42815632e
mcle0341 <mcle0341@algonquinlive.com>
2016-03-11 17:19:59 -0500

    added and removed space

--- js/main.js
+++ js/main.js
@@ -3,10 +3,19 @@ var $h1 = $('h1');
 var $main = $('main');
 var $p = $('p');
 var $h2 = $('h2');
+var $images = $('images');
+var $dl = $('dl');
+var $dt = $('dt');
 
-$header.addClass('.masthead');
-$h1.addClass('.logo');
-$main.addClass('.wrapper');
+$header.addClass('masthead');
+$h1.addClass('logo');
+$h1.removeClass('big-heading');
+$main.addClass('wrapper');
+$p.addClass('intro');
+$h2.addClass('slug-head');
+$images.addClass('slug-img');
+$dl.addClass('classification');
+$dt.addClass('big-label');
+.show('')
 
-$('.big-heading').remove();
 
