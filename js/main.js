43c7b840fe02d4820fe0294f5b47cebd7c328d64
mcle0341 <mcle0341@algonquinlive.com>
2016-03-11 17:00:43 -0500

    added selecting variables

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $p = $('p');
+var $h2 = $('h2');
+
+$header.addClass('.masthead');
+$h1.addClass('.logo');
+$main.addClass('.wrapper');
+
+$('.big-heading'). remove();
